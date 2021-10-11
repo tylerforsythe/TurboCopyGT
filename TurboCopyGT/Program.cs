776a3cc81939b3b9973ab6118cf5dbafe5b64c18
Tyler Forsythe <forsythe.tyler@gmail.com>
2021-10-11 09:43:16 -0600

    Added intelligence to picking the number of threads. Started adding testing parameters for things like file and directory exclusions (not completed). Condensed output somewhat. Deleted some garbage code.

--- TurboCopyGT/Program.cs
+++ TurboCopyGT/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Enumeration;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Amib.Threading;
 
 namespace TurboCopyGT
@@ -10,21 +12,35 @@ namespace TurboCopyGT
     {
         private static SmartThreadPool _smartThreadPool;
 
-        private const int TOTAL_THREADS = 28;
+        private static readonly int TOTAL_THREADS = (int)Math.Round(Environment.ProcessorCount * 2.6);
 
         static void Main(string[] args) {
-
-            Console.WriteLine("Hello World!");
+            var testMode = false;
+            //Console.WriteLine("Hello World!");
             _smartThreadPool = new SmartThreadPool();
             _smartThreadPool.MaxThreads = TOTAL_THREADS;
 
             var details = new CopyDetails();
-            details.SourcePath = @"C:\temp\mblf-node-modules-20210625";
-            details.DestPath = @"H:\temp\mbl_build\copy-test-destination";
+            if (args != null && args.Length == 2) {
+                testMode = false;
+                details.SourcePath = args[0];
+                details.DestPath = args[1];
+            }
+            else {
+                Console.WriteLine("NO ARGS -- TEST MODE");
+                testMode = true;
+                details.SourcePath = @"C:\temp\mblf-node-modules-20210625";
+                details.DestPath = @"H:\temp\mbl_build\copy-test-destination";
+            }
 
-            // delete everything in dest
-            Console.WriteLine("Deleting dest (this is for testing only)");
-            DeleteEverythingInDirectory(details.DestPath);
+            // figure out to read arguments in a new way so that we can do things like -xf and -xd for excluded files & directories [list]
+            // when reading the X-files and dirs lists, if each individual entry does not contain a * or ?, then add * to the start and end automatically.
+
+            if (testMode) {
+                // delete everything in dest
+                Console.WriteLine("Deleting dest (this is for testing only)");
+                DeleteEverythingInDirectory(details.DestPath);
+            }
 
             var startDt = DateTime.Now;
 
@@ -36,7 +52,7 @@ namespace TurboCopyGT
             CreateAllDirectories(details);
 
             // copy all the files in a multi-threaded fashion
-            Console.WriteLine("Start copy-threading...");
+            Console.WriteLine($"Starting copy-threading with {TOTAL_THREADS} threads");
             CopyAllFilesLimitedThreads(details);
             Console.WriteLine("Waiting for copy-threads to finish");
 
@@ -47,22 +63,26 @@ namespace TurboCopyGT
 
             var endDt = DateTime.Now;
             var duration = endDt - startDt;
-            Console.WriteLine($"Operation Stats:");
-            Console.WriteLine($"Files: {details.Files.Count:N0}");
-            Console.WriteLine($"Directories: {details.Directories.Count:N0}");
-            Console.WriteLine($"Total Bytes: {details.TotalBytes:N0}");
-            Console.WriteLine($"Bytes Formatted: {SizeSuffix(details.TotalBytes)}");
-            Console.WriteLine($"Duration: {duration}");
+            Console.WriteLine($"Operation Stats... Duration: {duration}");
+            Console.WriteLine($"Files: {details.Files.Count:N0}; Directories: {details.Directories.Count:N0}");
+            Console.WriteLine($"Total Bytes: {details.TotalBytes:N0}; {SizeSuffix(details.TotalBytes)}");
 
             // 20-24 seconds, 90-98 MB/s
 
             var totalSeconds = (long)duration.TotalSeconds;
-            var bytesPerSecond = details.TotalBytes / totalSeconds;
+            var bytesPerSecond = details.TotalBytes / Math.Max(totalSeconds, 1);
             Console.WriteLine($"{bytesPerSecond:N0} bytes/sec aka {SizeSuffix(bytesPerSecond)}/sec");
 
-            Console.WriteLine($"DONE");
+            Console.WriteLine($"DONE with copy: {details.SourcePath} => {details.DestPath}");
         }
 
+        /// <summary>
+        /// This splits all the files into TOTAL_THREADS groups. It does not take into account file sizes, just file counts.
+        /// It is faster to spin up TOTAL_THREADS threads and then let them go, rather than overhead of starting and ending
+        /// threads over and over again. Spin up and down the threads just once.
+        /// In my first iteration, I did one thread per file and determined it spent ~35% of run-time in thread overhead.
+        /// </summary>
+        /// <param name="details"></param>
         private static void CopyAllFilesLimitedThreads(CopyDetails details) {
             Console.WriteLine($"CopyAllFiles: {details.Files.Count:N0}");
             var filesPerThread = (details.Files.Count / TOTAL_THREADS) + 1;
@@ -73,27 +93,7 @@ namespace TurboCopyGT
                 p.EndIndex = ((i + 1) * filesPerThread) - 1;
                 p.CopyDetails = details;
                 p.ThreadNumber = i;
-                _smartThreadPool.QueueWorkItem(TurboCopyGT.Program.CopyFileBatch, p);
-            }
-        }
-
-        /// <summary>
-        /// This is how I first wrote it, just as a basic test, and it was super-fast.
-        /// However, I think I'm going to drop it because, despite being *super* straight-forward,
-        /// there is a ton of overhead in a thread for every single File.Copy.
-        /// In my testing, I cut 22s to 16s. Same files, but 35% of time in thread overhead.
-        /// </summary>
-        private static void CopyAllFilesThreadForEach(CopyDetails details) {
-            Console.WriteLine($"CopyAllFiles: {details.Files.Count:N0}");
-
-            foreach (var sourceFilePath in details.Files) {
-                var destFilePath = sourceFilePath.Replace(details.SourcePath, details.DestPath);
-                if (sourceFilePath == destFilePath) // should never happen!?!
-                    continue;
-
-                var fi = new FileInfo(sourceFilePath);
-                details.TotalBytes += fi.Length;
-                _smartThreadPool.QueueWorkItem(System.IO.File.Copy, sourceFilePath, destFilePath);
+                _smartThreadPool.QueueWorkItem(TurboCopyGT.Program.CopyFileBatch, p, WorkItemPriority.BelowNormal);
             }
         }
 
@@ -136,14 +136,46 @@ namespace TurboCopyGT
         }
 
         public static void ScanForAllDirectoriesAndFiles(string path, ref CopyDetails details) {
+            // these are Windows-specific directories that appear at *disk root*.
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                var isDiskRoot = IsPathAtDiskRoot(path);
+                if (isDiskRoot && (path.EndsWith("System Volume Information") ||
+                                   path.EndsWith("$RECYCLE.BIN") ||
+                                   path.EndsWith("Recovery")))
+                    return;
+            }
+
             System.IO.DirectoryInfo di = new DirectoryInfo(path);
             foreach (FileInfo file in di.GetFiles()) {
+                if (DoAnyPatternsMatch(details.ExcludedFilePatterns, file.FullName))
+                    continue;
                 details.Files.Add(file.FullName);
             }
             foreach (DirectoryInfo dir in di.GetDirectories()) {
+                if (DoAnyPatternsMatch(details.ExcludedDirectoryPatterns, dir.FullName))
+                    continue;
                 details.Directories.Add(dir.FullName);
                 ScanForAllDirectoriesAndFiles(dir.FullName, ref details);
             }
+        }
+
+        private static bool DoAnyPatternsMatch(List<string> patterns, string strToCheck) {
+            if (patterns == null || patterns.Count == 0)
+                return false;
+            return patterns.Any(exp => FileSystemName.MatchesSimpleExpression(exp, strToCheck));
+        }
+
+        /// <summary>
+        /// Found FileSystemName.MatchesSimpleExpression @ https://stackoverflow.com/a/66465594/7656
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static bool IsPathAtDiskRoot(string path) {
+            if (path.StartsWith(@"\\") && path.Count(c => c == '\\') == 3)
+                return true;
+            if (FileSystemName.MatchesSimpleExpression(@"?:\", path) && path.Count(c => c == '\\') == 1)
+                return true;
+            return false;
         }
 
         static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
@@ -169,6 +201,9 @@ namespace TurboCopyGT
                 Files = new List<string>();
                 TotalBytes = 0;
                 BytesWrittenPerThread = new long[TOTAL_THREADS];
+                
+                ExcludedDirectoryPatterns = new List<string>();
+                ExcludedFilePatterns = new List<string>();
             }
 
             public List<string> Directories { get; set; }
@@ -180,6 +215,9 @@ namespace TurboCopyGT
             public long TotalBytes { get; set; }
 
             public long[] BytesWrittenPerThread { get; private set; }
+
+            public List<string> ExcludedDirectoryPatterns { get; set; }
+            public List<string> ExcludedFilePatterns { get; set; }
 
             public long TotalBytesWrittenAllThreads() {
                 return BytesWrittenPerThread.Sum();
